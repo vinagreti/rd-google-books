b0baa3d10474d6bae33eaa5e3e11b1c72953839b
Bruno da Silva João <vinagreti@users.noreply.github.com>
2017-07-18 14:34:56 -0600

    Build FOR GH PAGES

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
- 
+ng build --prod --aot --output-path="docs" --base-href="./"
+
 git add --all
 git add -u
 git commit -m "Build FOR GH PAGES"
-ng build --prod --aot --output-path="docs" --base-href="./"
-
 git push
