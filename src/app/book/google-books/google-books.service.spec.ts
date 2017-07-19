00e6839af4e794aca13314462d96ae01e9487f90
Bruno da Silva João <vinagreti@users.noreply.github.com>
2017-07-18 18:07:38 -0600

    Tests ok

--- src/app/book/google-books/google-books.service.spec.ts
+++ src/app/book/google-books/google-books.service.spec.ts
@@ -19,36 +19,4 @@ describe('GoogleBooksService', () => {
 		expect(service).toBeTruthy();
 	}));
 
-
-	it('Should pesquisar um livro por palavra-chave e ver o resultado na mesma página', inject([GoogleBooksService], 
-		(service: GoogleBooksService) => {
-
-		})
-	);
-
-	it('Should conseguir fazer paginação do resultado da pesquisa', inject([GoogleBooksService], 
-		(service: GoogleBooksService) => {
-
-		})
-	);
-
-	it('Should marcar um livro como favorito', inject([GoogleBooksService], 
-		(service: GoogleBooksService) => {
-
-		})
-	);
-
-	it('Should clicar em um livro na listagem e ver uma página com mais informações do livro', inject([GoogleBooksService], 
-		(service: GoogleBooksService) => {
-
-		})
-	);
-
-	it('Should que a palavra-chave pesquisada fosse destacada no resultado da pesquisa', inject([GoogleBooksService], 
-		(service: GoogleBooksService) => {
-
-		})
-	);
-
-
 });
